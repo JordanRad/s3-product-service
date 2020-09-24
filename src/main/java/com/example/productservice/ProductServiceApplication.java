a44a00708e7838d6aa0e35c96cdf60cb6236fe3d
= <=>
2020-09-24 10:21:17 +0200

    product service init

---
+++ src/main/java/com/example/productservice/ProductServiceApplication.java
@@ -0,0 +1,19 @@
+package com.example.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EnableEurekaClient
+public class ProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+
+}
