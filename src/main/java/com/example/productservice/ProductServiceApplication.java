02d8808d94f2ef744e24967ef477ee9a51fffbda
= <=>
2020-12-10 12:45:17 +0100

    Tests

--- src/main/java/com/example/productservice/ProductServiceApplication.java
+++ src/main/java/com/example/productservice/ProductServiceApplication.java
@@ -11,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableJpaAuditing
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
